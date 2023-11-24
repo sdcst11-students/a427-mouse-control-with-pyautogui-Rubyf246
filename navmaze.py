888b56534beb343b715ed6d239ad92827295ac13
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2023-11-24 13:10:25 -0800

    huh

--- navmaze.py
+++ navmaze.py
@@ -2,13 +2,25 @@
 import pyautogui
 import time
 import pyautogui, sys
-print('Press Ctrl-C to quit.')
-try:
-    while True:
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
-except KeyboardInterrupt:
-    print('\n')
 
+pyautogui.moveTo(1007, 200,3)
+pyautogui.moveTo(1082,207,3)
+pyautogui.moveTo(1085,272,3)
+pyautogui.moveTo(1057,277,3)
+pyautogui.moveTo(1057,373,3)
+pyautogui.moveTo(1105,372,3)
+pyautogui.moveTo(1105,351,3)
+pyautogui.moveTo(1084,348,3)
+pyautogui.moveTo(1087,300,3)
+pyautogui.moveTo(1131, 295,3)
+pyautogui.moveTo(1133,252,3)
+pyautogui.moveTo(1178,251,3)
+pyautogui.moveTo(1182,223,3)
+pyautogui.moveTo(1228,230,3)
+pyautogui.moveTo(1229,272,3)
+pyautogui.moveTo(1177,274,3)
+pyautogui.moveTo(1153,350,3)
+pyautogui.moveTo(1163,421,3)
+pyautogui.moveTo(1207,418,3)
+pyautogui.moveTo(1202,398,3)
+pyautogui.moveTo(1311,396,3)
